85dd2b05ffb9139723ac2dcf90363a9abb4e3b34
akash zone <akash97715@gmail.com>
2019-09-07 22:22:38 +0530

    Added jobs and project

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+#create a blog model
+
+#title
+#pub_date
+#body
+#image
+
+class Blog(models.Model):
+    title =  models.CharField(max_length=255)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
+
+#add the blog app in setting
+
+#create a migration
+
+#migrate
+
+#add to the admin
+
